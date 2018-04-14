e11ec7c7fa0fe4523a03b592446d52928323f2c7
David Meister <thedavidmeister@gmail.com>
2018-04-14 19:26:20 +1000

    javelin cell support

--- src/elem_lib/hoplon.cljs
+++ src/elem_lib/hoplon.cljs
@@ -5,6 +5,17 @@
   camel-snake-kebab.core
   syntax-highlighter.hoplon))
 
+(defn code
+ [code-cell]
+ (j/with-let [code-block (h/code)]
+  (code-block
+   :class/elem-lib "clojure"
+   (j/formula-of
+    [code-cell]
+    (h/with-animation-frame (syntax-highlighter.hoplon/highlight! code-block))
+    ; cells of cells are common in examples
+    (pr-str code-cell)))))
+
 (defn elem
  [name desc f examples]
  (h/div
@@ -16,13 +27,12 @@
   (h/for-tpl [[d & args] examples]
    (h/div :class/elem-lib "elem-lib-example"
     (h/h3 d)
+    "Arguments:"
     (h/for-tpl [arg args]
-     (j/with-let [code-block (h/code)]
-      (code-block
-       :class/elem-lib "clojure"
-       (let [arg' @arg]
-        (j/formula-of
-         [arg']
-         (h/with-animation-frame (syntax-highlighter.hoplon/highlight! code-block))
-         (pr-str @arg))))))
-    (apply @f @args)))))
+     (code @arg))
+    (let [result (apply @f @args)]
+     (if (j/cell? result)
+      (h/div
+       "Javelin cell value:" (h/br)
+       (code result))
+      result))))))
